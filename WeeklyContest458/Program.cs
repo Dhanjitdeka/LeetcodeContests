0b4d590721a75aec1478f565bd6b51d441399a8a
Dhanjit Deka <dhanjit1951@gmail.com>
2025-07-13 21:06:24 +0530

    Basic code setup for the contest

--- WeeklyContest458/Program.cs
+++ WeeklyContest458/Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Solution 
+{
+    // Problem 1: Replace with actual problem
+    public int Problem1() 
+    {
+        // Implementation here
+        return 0;
+    }
+    
+    // Problem 2: Replace with actual problem
+    public string Problem2() 
+    {
+        // Implementation here
+        return "";
+    }
+    
+    // Problem 3: Replace with actual problem
+    public bool Problem3() 
+    {
+        // Implementation here
+        return false;
+    }
+    
+    // Problem 4: Replace with actual problem
+    public int Problem4() 
+    {
+        // Implementation here
+        return 0;
+    }
+}
+
+// Helper class for testing
+public class Program 
+{
+    public static void Main(string[] args) 
+    {
+        Solution solution = new Solution();
+        
+        // Test Problem 1
+        
+        Console.WriteLine($"Problem 1 Result: {solution.Problem1()}");
+        
+        // Test Problem 2
+        
+        Console.WriteLine($"Problem 2 Result: {solution.Problem2()}");
+        
+        // Test Problem 3
+        Console.WriteLine($"Problem 3 Result: {solution.Problem3()}");
+        
+        // Test Problem 4
+       
+        Console.WriteLine($"Problem 4 Result: {solution.Problem4()}");
+    }
+}
