df906357b13f06d8f54df6f4645fc04237a751d4
Dhanjit Deka <dhanjit1951@gmail.com>
2025-07-13 21:42:55 +0530

    Solved problem 1

--- WeeklyContest458/Program.cs
+++ WeeklyContest458/Program.cs
@@ -2,14 +2,49 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+//https://leetcode.com/problems/process-string-with-special-operations-i/
 public class Solution 
 {
     // Problem 1: Replace with actual problem
-    public int Problem1() 
+    public String processStr(String s)  
     {
-        // Implementation here
-        return 0;
+        // In C#, StringBuilder is used when you need to manipulate strings repeatedly or perform many string concatenations. 
+        // This is because strings in C# are immutable, meaning every time you modify a string, 
+        // you're actually creating a brand new one in memory. 
+        StringBuilder result = new StringBuilder();
+
+        foreach (char c in s.ToCharArray())
+        {
+            if (c <= 'z' && c >= 'a')
+            {
+                result.Append(c);
+            }
+            else if (c == '*')
+            {
+                if (result.Length > 0)
+                {
+                    result.Remove(result.Length - 1, 1);
+                }
+            }
+            else if (c == '#')
+            {
+                // no need to use result.toString() inside the append method
+                result.Append(result);
+            }
+            else if (c == '%')
+            {
+                char[] charArray = result.ToString().ToCharArray();
+                Array.Reverse(charArray);
+                // made a rookie mistake by doing charArray.toString()
+                // it does not return a string version of the array as I thought.
+                // By default, Object.ToString() returns the type name (unless overridden)
+                // StringBuilder overrides ToString() to return the actual string it's building
+                result = new StringBuilder(new string(charArray));
+                
+            }
+        }
+        
+        return result.ToString();
     }
     
     // Problem 2: Replace with actual problem
@@ -43,7 +78,7 @@ public class Program
         
         // Test Problem 1
         
-        Console.WriteLine($"Problem 1 Result: {solution.Problem1()}");
+        Console.WriteLine($"Problem 1 Result: {solution.processStr("a#b%*")}");
         
         // Test Problem 2
         
